6792edb12d89f142cf0bd2c71c75f0cfbda28e01
Raúl Marín <rmarin82@gmail.com>
2020-03-24 11:35:49 +0100

    Update server port.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8001;
+const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
